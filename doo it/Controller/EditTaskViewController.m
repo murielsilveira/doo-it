424f707b39c89a85f6187d2e74754fe38ae263ea
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-03-27 12:09:53 -0300

    Project organization improvement

---
+++ doo it/Controller/EditTaskViewController.m
@@ -0,0 +1,37 @@
+//
+//  EditTaskViewController.m
+//  doo it
+//
+//  Created by Ricardo Gehrke Filho on 27/03/16.
+//  Copyright © 2016 Ricardo Gehrke Filho. All rights reserved.
+//
+
+#import "EditTaskViewController.h"
+
+@interface EditTaskViewController ()
+
+@end
+
+@implementation EditTaskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
